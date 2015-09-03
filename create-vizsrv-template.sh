6430a1aaa946321578de5791bae50289f7941e62
zubatyuk <zubatyuk@gmail.com>
2015-09-02 19:44:34 -0500

    Update create-vizsrv-template.sh

--- create-vizsrv-template.sh
+++ create-vizsrv-template.sh
@@ -45,6 +45,49 @@ exit 0
 EOF
 chmod +x $lxcroot/etc/rc.local
 
+cat > $lxcroot/etc/network/interfaces << EOF
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet manual
+
+auto br-lan
+iface br-lan inet static
+  bridge_stp off
+  bridge_fd 0
+  address 192.168.0.2
+  netmask 192.268.240.0
+  gateway 192.168.0.1
+  dns-nameservers 192.168.0.1
+  
+##auto mveth0
+##iface mveth0 inet manual
+##  pre-up "ip link add dev mveth0 link eth0 type macvlan"
+##  post-down "ip link delete mveth0"
+#
+##auto eth0.2
+##iface eth0.2 inet manual
+#
+#auto br-wan
+#iface br-wan inet dhcp
+#  bridge_stp off
+#  bridge_fd 0
+##  bridge-ports mveth0
+##  bridge-ports eth0.2
+
+##auto eth1
+##iface eth1 inet manual
+#
+#auto br-hp
+#iface br-hpn inet static
+#  bridge_stp off
+#  bridge_fd 0
+#  bridge-ports eth1
+#  address 192.168.16.1
+#  netmask 192.168.240.0
+EOF
+
 lxc-stop -n srvtmpl
 sleep 5
 
