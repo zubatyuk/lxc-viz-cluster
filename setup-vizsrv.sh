cd7f92c8b0745175c48f2153acb3dad7fa443f40
zubatyuk <zubatyuk@gmail.com>
2015-09-03 08:15:07 -0500

    Update setup-vizsrv.sh

--- setup-vizsrv.sh
+++ setup-vizsrv.sh
@@ -17,6 +17,11 @@ apt-get update && apt-get -y install lxc
 apt-add-repository -y ppa:xorg-edgers/ppa
 apt-get update && apt-get -y install nvidia-355 bumblebee
 
+#upgrades
+apt-get -y install unattended-upgrades
+echo "APT::Periodic::Update-Package-Lists \"1\";" > /etc/apt/apt.conf.d/22auto-upgrade
+echo "APT::Periodic::Unattended-Upgrade \"1\";" >> /etc/apt/apt.conf.d/22auto-upgrade
+
 #timezone
 echo 'US/Central' > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
