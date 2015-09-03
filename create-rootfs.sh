d49679350f8a0eeebe5ec8087cdd4d269080b9ac
zubatyuk <zubatyuk@gmail.com>
2015-09-02 21:08:07 -0500

    Update create-rootfs.sh

--- create-rootfs.sh
+++ create-rootfs.sh
@@ -16,12 +16,12 @@ apt-get -y install lxc
 lxc-create -t download -n srvtmpl -- --dist ubuntu --release trusty --arch amd64
 lcxroot=/var/lib/lxc/srvtmpl/rootfs
 echo "lxc.aa_profile = unconfined" >> /var/lib/lxc/srvtmpl/config
-lxc-start -n srvtmpl -d
-sleep 5
 
-lxc-attach -n srvtmpl << EOF
+mkdir $lxcroot/root/scripts
+cat > $lxcroot/root/scripts/init.sh << EOF
+#!/bin/bash
 apt-get update
-apt-get -y install linux-generic-lts-vivid vlan bridge-utils tasksel grub2
+apt-get -y install linux-generic-lts-vivid vlan bridge-utils tasksel grub2 acpid
 tasksel install server
 tasksel install openssh-server
 apt-get clean
@@ -29,6 +29,12 @@ rm /etc/ssh/ssh_host_*
 perl -i -pe "s/^PermitRootLogin without-password/PermitRootLogin yes/" /etc/ssh/sshd_config
 usermod --pass='$1$eXo/XRoW$sED.Q4FS5e6kbbtJyQFBY.' root
 EOF
+
+lxc-start -n srvtmpl -d && lxc-wait -n srvtmpl -s RUNNING
+sleep 20
+lxc-attach -- bash /root/scripts/init.sh
+lxc-stop -n srvtmpl && lxc-wait -n srvtmpl -s STOPPED
+sleep 5
 
 cat > $lxcroot/etc/rc.local << EOF
 #!/bin/bash
@@ -81,9 +87,6 @@ iface br-lan inet static
 #  address 192.168.16.1
 #  netmask 192.168.240.0
 EOF
-
-lxc-stop -n srvtmpl
-sleep 5
 
 cd $lxcroot
 tar --acls --xattrs --keep-directory-symlink --numeric-owner --selinux -czf $archive * 
